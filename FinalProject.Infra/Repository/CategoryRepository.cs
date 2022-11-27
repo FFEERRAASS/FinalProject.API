7344a57392bc3bdd9f5e0da9d2388ec94ad03e6a
aymanSE <61785906+aymanSE@users.noreply.github.com>
2022-11-27 19:20:19 +0300

    AYMAN CATNOTCOMPLETE

---
+++ FinalProject.Infra/Repository/CategoryRepository.cs
@@ -0,0 +1,43 @@
+﻿using FinalProject.Core.Common;
+using FinalProject.Core.Data;
+using FinalProject.Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProject.Infra.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly IDbContext dbContext;
+        public CategoryRepository(IDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void CREATECategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAllCategory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetCategoryById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UPDATECategory(int id, Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
