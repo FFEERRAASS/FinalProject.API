f499f4f9f46718b89a17c8c61dc1265ddbb573e6
aymanSE <61785906+aymanSE@users.noreply.github.com>
2022-11-27 20:07:38 +0300

    aaadd

--- FinalProject.API/Controllers/UsersController.cs
+++ FinalProject.API/Controllers/UsersController.cs
@@ -24,7 +24,7 @@ namespace FinalProject.API.Controllers
             return userService.GetAllUsers();
         }
         [HttpPost]
-        [Route("CreateUser")] ///////
+        [Route("CreateUser")] ///////aaa
         public void CreateUser(User user)
         {
             userService.CreateUser(user);
